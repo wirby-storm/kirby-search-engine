9a6dec5a725d7086a95d1f54acfb68abdd026137
Hoppin Mad Bunny <161526456+hoppinmadbunny@users.noreply.github.com>
2024-07-10 14:47:29 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ const re = document.getElementById('q')
 
 function dodo (){
   const input = document.getElementById('u').value
-
-  window.open(`https://www.google.com/search?q=${input}`);
+  if input = "wirby maps"{  
+  }
+  else{
+  
+    window.open(`https://www.google.com/search?q=${input}`);
+  }
 }
 let arr = ["mirr.jpeg", "asset.jpeg","jpeg.jpeg", "kirby6.jpeg", "kirbyforgoot.jpeg", "mirr2.jpeg","kirby0.jpeg", "robot.png", "wii.jpeg", "maxresdefault.jpg", "KirbySuperStar.jpeg", "max.jpeg", "air.png", "Blowout_Blast.png", "d.jpg", "snes.jpeg", "maxres.jpeg", "kirbydrame.jpeg", "DreamCollectionF.jpg", "bat.png"];
 document.body.style.backgroundImage = `url(${arr[(Math.floor(Math.random() * arr.length))]})`;
