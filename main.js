100e9fa3164a3103e31d8fd21c3d09d449293f10
Hoppin Mad Bunny <161526456+hoppinmadbunny@users.noreply.github.com>
2024-07-10 15:45:48 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const re = document.getElementById('q')
 
 function dodo (){
   const input = document.getElementById('u').value
-    // window.open(`https://wirbymaps.pages.dev/`);
+
 
   
-    window.open(`https://www.google.com/search?q=${input}`);
+  window.open(`https://www.google.com/search?q=${input}`);
 
 }
 let arr = ["mirr.jpeg", "asset.jpeg","jpeg.jpeg", "kirby6.jpeg", "kirbyforgoot.jpeg", "mirr2.jpeg","kirby0.jpeg", "robot.png", "wii.jpeg", "maxresdefault.jpg", "KirbySuperStar.jpeg", "max.jpeg", "air.png", "Blowout_Blast.png", "d.jpg", "snes.jpeg", "maxres.jpeg", "kirbydrame.jpeg", "DreamCollectionF.jpg", "bat.png"];
