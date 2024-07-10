8fc70a7a4ad8618ea2f95669d7651e9f89edef7b
Hoppin Mad Bunny <161526456+hoppinmadbunny@users.noreply.github.com>
2024-07-10 14:42:48 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const button = document.getElementById('dod')
+const re = document.getElementById('q')
+
+function dodo (){
+  const input = document.getElementById('u').value
+
+  window.open(`https://www.google.com/search?q=${input}`);
+}
+let arr = ["mirr.jpeg", "asset.jpeg","jpeg.jpeg", "kirby6.jpeg", "kirbyforgoot.jpeg", "mirr2.jpeg","kirby0.jpeg", "robot.png", "wii.jpeg", "maxresdefault.jpg", "KirbySuperStar.jpeg", "max.jpeg", "air.png", "Blowout_Blast.png", "d.jpg", "snes.jpeg", "maxres.jpeg", "kirbydrame.jpeg", "DreamCollectionF.jpg", "bat.png"];
+document.body.style.backgroundImage = `url(${arr[(Math.floor(Math.random() * arr.length))]})`;
