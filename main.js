b9f5ac308a0e8a7b3143f200ce8513f1fba6af0f
Hoppin Mad Bunny <161526456+hoppinmadbunny@users.noreply.github.com>
2024-07-11 12:03:21 -0600

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ document.getElementById('u').onkeydown = function(event) {
     }
 }
 document.getElementById('myImage').onclick() = funtion(){
-    console.log('doen)
+    console.log('doen')
 }
 let arr = ["mirr.jpeg", "asset.jpeg","jpeg.jpeg", "kirby6.jpeg", "kirbyforgoot.jpeg", "mirr2.jpeg","kirby0.jpeg", "robot.png", "wii.jpeg", "maxresdefault.jpg", "KirbySuperStar.jpeg", "max.jpeg", "air.png", "Blowout_Blast.png", "d.jpg", "snes.jpeg", "maxres.jpeg", "kirbydrame.jpeg", "DreamCollectionF.jpg", "bat.png"];
 document.body.style.backgroundImage = `url(${arr[(Math.floor(Math.random() * arr.length))]})`;sel
