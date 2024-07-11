5f36860c3635ab747bfcf094c48371af8e106a52
Hoppin Mad Bunny <161526456+hoppinmadbunny@users.noreply.github.com>
2024-07-11 14:46:56 -0600

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ document.getElementById('u').onkeydown = function(event) {
     }
 }
 function done(){
-    console.log('doen')
+    window.open(`https://wirby.pages.dev/`);
 }
 let arr = ["mirr.jpeg", "asset.jpeg","jpeg.jpeg", "kirby6.jpeg", "kirbyforgoot.jpeg", "mirr2.jpeg","kirby0.jpeg", "robot.png", "wii.jpeg", "maxresdefault.jpg", "KirbySuperStar.jpeg", "max.jpeg", "air.png", "Blowout_Blast.png", "d.jpg", "snes.jpeg", "maxres.jpeg", "kirbydrame.jpeg", "DreamCollectionF.jpg", "bat.png"];
 document.body.style.backgroundImage = `url(${arr[(Math.floor(Math.random() * arr.length))]})`;
